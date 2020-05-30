101ffc2198a251a7629685a10779bcf3b03c6f5f
Rajat <rajatarya199zx@gmail.com>
2020-05-30 15:28:01 +0530

    logging user in

--- script/auth.js
+++ script/auth.js
@@ -23,6 +23,9 @@ signupform.addEventListener('submit',(e)=>{
 
 })
 
+
+//logout
+
 const logout= document.querySelector('#logout');
 
 logout.addEventListener('click',(e)=>{
@@ -31,3 +34,27 @@ logout.addEventListener('click',(e)=>{
        ()=>console.log("log out ")
    )
 })
+
+//login
+const loginForm=document.querySelector('#login-form');
+loginForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    
+    
+    
+    //get user info
+    const email=loginForm['login-email'].value;
+    const password=loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(email,password).then(
+        (cred)=>{
+            console.log(cred.user)
+            //close the login form and reset the form
+            const modal=document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset()
+        }
+    )
+})
+
+
+
